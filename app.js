6f006f2bbc7801662c94d45fe427715e24d2eac7
danielcortes <dcortes@velocitypartners.net>
2018-01-29 17:28:56 -0500

    creating new commands

--- app.js
+++ app.js
@@ -4,28 +4,43 @@ const command = args._[0];
 
 const notes = require('./notes.js');
 
-if(command === 'list'){
-    notes.getAll();
-}
-else if(command === 'add'){
-    var note = notes.addNote(args.title, args.body); 
-    if(note){
-        console.log('Note added!', note);
-    }
-    else{
-        console.log('ERROR: Duplicated note!');
-    }
-}
-else if(command === 'read'){ 
-    notes.getNote(args.title)
-}
-else if(command === 'remove'){
-    var noteRemoved = notes.removeNote(args.title);
+// if(command === 'list'){
+//     notes.getAll();
+// }
+// else if(command === 'add'){
+//     var note = notes.addNote(args.title, args.body); 
+//     if(note){
+//         console.log('Note added!', note);
+//     }
+//     else{
+//         console.log('ERROR: Duplicated note!');
+//     }
+// }
+// else if(command === 'read'){ 
+//     notes.getNote(args.title)
+// }
+// else if(command === 'remove'){
+//     var noteRemoved = notes.removeNote(args.title);
 
-    if(noteRemoved){
-        console.log('Note Removed')
+//     if(noteRemoved){
+//         console.log('Note Removed')
+//     }
+//     else{
+//         console.log('Note not found')
+//     }
+// }
+
+
+const commands = {
+    "run-test": function(){
+        if(!args.machine || !args.test){
+            console.log('Error: Machine serial or test name are missing!');
+        }else{
+            
+        }
     }
-    else{
-        console.log('Note not found')
-    }
+}
+
+if(command){
+    commands[command]();
 }
